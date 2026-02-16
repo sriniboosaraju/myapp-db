46a70d612af554daeb44a32eccb10bef53bedfa4
Srinivas Boosaraju <srinivas.boosaraju@octanner.com>
2026-02-16 07:47:15 +0530

    chore: force pod restart with SSL-enabled image

--- app/index.js
+++ app/index.js
@@ -2,7 +2,6 @@ const express = require('express');
 const { Pool } = require('pg');
 const app = express();
 const port = process.env.PORT || 3000;
-// Force rebuild after making GHCR public
 
 // PostgreSQL connection pool
 const pool = new Pool({
