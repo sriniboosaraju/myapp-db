97fdd986999ba2ea7f767ea5e659ede2d84e1678
Srinivas Boosaraju <srinivas.boosaraju@octanner.com>
2026-03-09 13:31:34 +0530

    changed tag

--- infra/ec2/main.tf
+++ infra/ec2/main.tf
@@ -153,7 +153,7 @@ resource "aws_instance" "app" {
   }))
 
   tags = {
-    Name        = "Demo-oct-ec2"
+    Name        = "oct-ec2"
     Environment = var.environment
     AppName     = var.app_name
     ManagedBy   = "Terraform"
